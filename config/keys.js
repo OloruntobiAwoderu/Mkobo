34680c9c254462dec8ca1f6a6f964a2b5d5f5640
oloruntobi <awoderuoloruntobi@gmail.com>
2020-06-18 11:59:56 +0100

    Refactored keys and reset password

--- config/keys.js
+++ config/keys.js
@@ -5,9 +5,6 @@ module.exports = {
   DATA_DB: process.env.MONGOLAB_URI,
   JWTSecret: process.env.JWT_Secret,
   FRONTEND: process.env.FRONTEND,
-  USER_MAIL: process.env.USER_MAIL,
-  PASSWORD_MAIL: process.env.PASSWORD_MAIL,
-  SENDGRID_USERNAME: process.env.SENDGRID_USERNAME,
-  SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
-  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  SENDERS_EMAIL: process.env.SENDERS_EMAIL
 };
